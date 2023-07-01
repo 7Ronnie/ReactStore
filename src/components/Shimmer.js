057f7805ff813ed0d2991cde654ae5f7539141cb
Kunal <kunalchoudhary619.kc@gmail.com>
2023-07-01 17:44:38 +0530

    Component added

---
+++ src/components/Shimmer.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function Shimmer() {
+  return (
+    <div className='shimmerCont'>
+        <div className="shimmerCard">
+        </div>
+        <div className="shimmerCard">
+        </div>
+        <div className="shimmerCard">
+        </div>
+        <div className="shimmerCard">
+        </div>
+        <div className="shimmerCard">
+        </div>
+        <div className="shimmerCard">
+        </div>
+        <div className="shimmerCard">
+        </div>
+        <div className="shimmerCard">
+        </div>
+        <div className="shimmerCard">
+        </div>
+        <div className="shimmerCard">
+        </div>
+        <div className="shimmerCard">
+        </div>
+    
+    </div>
+  )
+}
